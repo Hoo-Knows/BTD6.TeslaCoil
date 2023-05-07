4843432450f4c83367ece7ff0528136d5e14f2e9
Hoo-Knows <73412292+Hoo-Knows@users.noreply.github.com>
2023-05-07 13:57:41 -0700

    Update ModHelperData and build.yml

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -10,8 +10,8 @@ public static class ModHelperData
     public const string Description = "Adds a new Tack Shooter path with Paths++\n\n" +
 		                              "Thanks to everyone in the BTD Modding Discord who helped me with my first mod, in particular doombubbles and Onixiya whose code I referenced frequently.";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "Hoo-Knows";
+    public const string RepoName = "BTD6.TeslaCoil";
 
     public const string Dependencies = "doombubbles/paths-plus-plus";
 }
